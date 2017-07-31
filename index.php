ce1f7d1735e64dce1a03c595ae3afff9f9d0471a
Superd22 <superd001@gmail.com>
2017-07-31 07:58:10 +0200

    stage

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ header("Access-Control-Allow-Origin: https://juliet.starcitizen.fr");
 header("Access-Control-Allow-Credentials: true");
 header('Access-Control-Allow-Headers: Content-Type');
 
-error_reporting(-1);
+//error_reporting(-1);
 ini_set("display_errors", "1");
 $r3 = APIRouter::get_router();
 
--- ships/models/CrewPosition.php
+++ ships/models/CrewPosition.php
@@ -10,11 +10,13 @@ class CrewPosition {
     public $template_id;
     public $id;
     public $name;
+    public $parent;
     
     public function __construct($crew) {
         if($crew !== null && $crew['template_id'] !== null) {
             $this->id = (integer) $crew['id'];
             $this->template_id = (integer) $crew['template_id'];
+            $this->parent = (integer) $crew['parent'];
             $this->name = $crew['name'];
         }
         else throw new \Exception("INVALID CREW POSITION TARGET");
